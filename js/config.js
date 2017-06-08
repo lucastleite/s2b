6b0a38ab813c0e2064d26233474b1887b3f2df43
Lucas Leite <lucastleite@gmail.com>
2017-06-08 00:18:00 -0300

    third commit

---
+++ js/config.js
@@ -0,0 +1,1 @@
+// arquivo de configuração inicial JavaScript, contendo descrição dos arquivos JS e para que serve cada um deles
---
+++ sql/config.sql
@@ -0,0 +1,1 @@
+# Arquivo de configuração do banco de dados, contendo dados sobre a criação do banco de dados. Para cada tabela, deve ter um arquivo sql TABELA.sql
